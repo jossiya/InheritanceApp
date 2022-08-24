3ed59c0f9a22fe141babf133c1990824a6e284fa
jossi <rolety2202@naver.com>
2022-08-24 17:44:49 +0900

    상속과 생성자

--- Inheritance/src/InheritanceApp.java
+++ Inheritance/src/InheritanceApp.java
@@ -1,31 +1,30 @@
 class Cal{
-	public int sum(int v1,int v2) {
-		return v1+v2;
+	int v1,v2;
+	Cal(int v1,int v2){
+		System.out.println("Cal init!!");
+		this.v1=v1; this.v2=v2;
 	}
-	//Overloading 매서드 이름이 같아도 안에 조건이 다르면 사용 가능
-	public int sum(int v1,int v2,int v3) {
-		return this.sum(v1,v2)+v3;
+	public int sum() {
+		return this.v1+v2;
 	}
 }
 class Cal3 extends Cal{
-	public int minus(int v1,int v2) {
-		return v1-v2;
+
+	Cal3(int v1, int v2) {
+		super(v1, v2);
+		System.out.println("Cal3 init!!");
 	}
-	//부보가 가지고있는 매소드를 제정의 Overriding
-	public int sum(int v1,int v2) {
-		System.out.println("Cal3!!");
-		return super.sum(v1,v2);
+	public int minus() {
+		return this.v1-v2;
 	}
 }
 public class InheritanceApp {
 	public static void main(String[] args) {
-		Cal c= new Cal();
-		 System.out.println(c.sum(2,1));
-		 System.out.println(c.sum(2,1,1));
-		 
-		 Cal3 c3= new Cal3();
-		 System.out.println(c3.sum(2, 1));
-		 System.out.println(c3.minus(2,1));
+		Cal c= new Cal(2,1);
+		Cal3 c3= new Cal3(2,1);
+		System.out.println(c3.sum());//3
+		System.out.println(c3.minus());//1
+		
 	}
 }
 	
